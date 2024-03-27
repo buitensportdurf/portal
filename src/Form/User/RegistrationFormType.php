5d7d9b1fd37ec9e225c099c32cccfb200cfa0adf
tim <tim.veenendaal@gmail.com>
2024-03-27 23:33:06 +0100

    Add required checkbox for privacy in registration

--- src/Form/User/RegistrationFormType.php
+++ src/Form/User/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form\User;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -41,7 +43,17 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('email', EmailType::class, ['required' => false])
-            ->add('register', SubmitType::class);
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+                'label' => 'I agree to the privacy statement found below',
+            ])
+            ->add('register', SubmitType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
