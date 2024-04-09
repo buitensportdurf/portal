1bf5da63add17632d20be948c633c2f8bf8a88d6
tim <tim.veenendaal@gmail.com>
2024-04-09 22:54:54 +0200

    Hopefully fix timezones and transitions

--- src/Controller/Event/CalendarController.php
+++ src/Controller/Event/CalendarController.php
@@ -9,7 +9,11 @@ use App\Repository\Event\EventRepository;
 use App\Repository\Event\TagRepository;
 use App\Transformer\EventCalTransformer;
 use DateInterval;
+use DateTimeImmutable;
 use Eluceo\iCal\Domain\Entity\Calendar;
+use Eluceo\iCal\Domain\Entity\TimeZone;
+use Eluceo\iCal\Domain\Enum\TimeZoneTransitionType;
+use Eluceo\iCal\Domain\ValueObject\TimeZoneTransition;
 use Eluceo\iCal\Presentation\Factory\CalendarFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -56,6 +60,7 @@ class CalendarController extends AbstractController
     private function getCalendarResponse(array $events, string $name): Response
     {
         $calendar = new Calendar();
+        $calendar->addTimeZone($this->getTimeZone());
         foreach ($events as $event) {
             $calendar->addEvent($this->eventCalTransformer->transform($event));
         }
@@ -71,5 +76,27 @@ class CalendarController extends AbstractController
             'Content-Type' => 'text/calendar; charset=utf-8',
             'Content-Disposition' => 'attachment; filename="cal.ics"',
         ]);
+    }
+
+    /**
+     * @return TimeZone
+     */
+    public function getTimeZone(): TimeZone
+    {
+        return (new TimeZone('Europe/Amsterdam'))
+            ->addTransition(new TimeZoneTransition(
+                TimeZoneTransitionType::STANDARD(),
+                new DateTimeImmutable('2024-10-27T03:00:00'),
+                0,
+                3600,
+                'CET'
+            ))->addTransition(new TimeZoneTransition(
+                TimeZoneTransitionType::DAYLIGHT(),
+                new DateTimeImmutable('2023-03-31T02:00:00'),
+                3600,
+                0,
+                'CEST'
+            ))
+        ;
     }
 }
