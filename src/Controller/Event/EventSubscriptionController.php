10cfb02abfc5b52ef04b8e8cddea353d56a66627
tim <tim.veenendaal@gmail.com>
2024-04-10 02:54:07 +0200

    Implement subscription editing with voter

--- src/Controller/Event/EventSubscriptionController.php
+++ src/Controller/Event/EventSubscriptionController.php
@@ -6,6 +6,7 @@ use App\Entity\Event\Event;
 use App\Entity\Event\EventSubscription;
 use App\Repository\Event\EventSubscriptionRepository;
 use App\Security\Voter\EventSubscriptionVoter;
+use App\Security\Voter\EventVoter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,29 +18,32 @@ class EventSubscriptionController extends AbstractController
 {
     public function __construct(
         private readonly EventSubscriptionRepository $repository,
-    )
-    {
-    }
+    ) {}
 
     #[Route('/subscribe/{id}', name: '_subscribe')]
     public function subscribe(Request $request, Event $event): Response
     {
-        $this->denyAccessUnlessGranted(EventSubscriptionVoter::SUBSCRIBE, $event);
+        $this->denyAccessUnlessGranted(EventVoter::SUBSCRIBE, $event);
 
         $subscription = new EventSubscription();
-        $subscription->setCreatedDateNowNoSeconds()
+        $subscription
+            ->setCreatedDateNowNoSeconds()
             ->setEvent($event)
             ->setAmount(1)
-            ->setCreatedUser($this->getUser());
+            ->setCreatedUser($this->getUser())
+        ;
 
-        $form = $this->createFormBuilder($subscription)
-            ->add('amount');
+        $form = $this
+            ->createFormBuilder($subscription)
+            ->add('amount')
+        ;
 
         if ($this->isGranted('ROLE_EVENT_ADMIN')) {
             $form->add('createdUser');
         }
         $form->add('note')
-            ->add('Subscribe', SubmitType::class);
+             ->add('subscribe', SubmitType::class)
+        ;
         $form = $form->getForm();
         $form->handleRequest($request);
 
@@ -63,11 +67,37 @@ class EventSubscriptionController extends AbstractController
     #[Route('/unsubscribe/{id}', name: '_unsubscribe')]
     public function unsubscribe(EventSubscription $subscription): Response
     {
-        $this->denyAccessUnlessGranted(EventSubscriptionVoter::UNSUBSCRIBE, $subscription->getEvent());
+        $this->denyAccessUnlessGranted(EventVoter::UNSUBSCRIBE, $subscription->getEvent());
 
         $event = $subscription->getEvent();
         $this->repository->delete($subscription);
         $this->addFlash('success', sprintf('You have unsubscribed from %s', $event));
         return $this->redirectToRoute('event_event_show', ['id' => $event->getId()]);
+    }
+
+    #[Route('/{id}/edit', name: '_edit')]
+    public function edit(EventSubscription $subscription, Request $request): Response
+    {
+        $this->denyAccessUnlessGranted(EventSubscriptionVoter::EDIT, $subscription);
+
+        $form = $this
+            ->createFormBuilder($subscription)
+            ->add('amount')
+            ->add('note')
+            ->add('save', SubmitType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repository->save($subscription);
+            $this->addFlash('success', sprintf('Updated subscription to %s', $subscription->getEvent()));
+            return $this->redirectToRoute('event_event_show', ['id' => $subscription->getEvent()->getId()]);
+        }
+
+        return $this->render('event/subscription/edit.html.twig', [
+            'subscription' => $subscription,
+            'form' => $form->createView(),
+        ]);
     }
 }
