e7d5f911a05f1cf9570e9ac03e438a6abdf84437
tim <tim.veenendaal@gmail.com>
2025-01-08 22:14:27 +0100

    Add price indication for events

---
+++ migrations/Version20250108205239.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250108205239 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event ADD member_price INT NOT NULL, ADD guest_price INT NOT NULL');
+        $this->addSql('ALTER TABLE recurring_event ADD member_price INT NOT NULL, ADD guest_price INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recurring_event DROP member_price, DROP guest_price');
+        $this->addSql('ALTER TABLE event DROP member_price, DROP guest_price');
+    }
+}
