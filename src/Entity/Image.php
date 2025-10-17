0e8cc0a92a32429dd7ea00e60afd225daf360261
Tim <git@loken.nl>
2025-10-17 22:45:51 +0200

    Fix image datetype

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -6,6 +6,7 @@ use App\Repository\ImageRepository;
 use DateTime;
 use DateTimeImmutable;
 use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -16,10 +17,8 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 #[Vich\Uploadable]
 class Image
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private $id;
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column]
+    private ?int $id = null;
 
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
@@ -27,14 +26,14 @@ class Image
     #[Vich\UploadableField(mapping: 'images', fileNameProperty: 'imageName', size: 'imageSize')]
     private ?File $imageFile;
 
-    #[ORM\Column(type: 'string')]
+    #[ORM\Column]
     private ?string $imageName = null;
 
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column]
     private ?int $imageSize = null;
 
-    #[ORM\Column(type: 'datetime')]
-    private ?DateTimeInterface $updatedAt;
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
+    private ?DateTimeImmutable $updatedAt;
 
     public function __toString(): string
     {
@@ -45,7 +44,7 @@ class Image
     #[ORM\PreUpdate]
     public function updateTimestamp(): void
     {
-        $this->setUpdatedAt(new DateTime('now'));
+        $this->setUpdatedAt(new DateTimeImmutable());
     }
 
     public function getId(): ?int
@@ -69,7 +68,7 @@ class Image
         if (null !== $imageFile) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new DateTimeImmutable();
+            $this->setUpdatedAt(new DateTimeImmutable());
         }
     }
 
@@ -98,12 +97,12 @@ class Image
         return $this->imageSize;
     }
 
-    public function getUpdatedAt(): ?DateTimeInterface
+    public function getUpdatedAt(): ?DateTimeImmutable
     {
         return $this->updatedAt;
     }
 
-    public function setUpdatedAt(DateTimeInterface $updatedAt): self
+    public function setUpdatedAt(DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
 
